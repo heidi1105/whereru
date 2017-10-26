8cdb5625cf332f4abfe71759cd2dadf3d025892d
Sandy <beachwhale13@yahoo.com>
2017-10-26 14:48:05 -0700

    stuff

--- src/main/java/com/heidi/whereru/controllers/Users.java
+++ src/main/java/com/heidi/whereru/controllers/Users.java
@@ -171,36 +171,4 @@ public class Users {
 	    
 	    return "redirect:/employers/addLocation";
 	}
-//	public static GeoPoint getGeoPointFromAddress(@RequestParam("address")String address) {
-//		GeoPoint locationPoint = null;
-//        String addy = address.replaceAll(" ", "%20");
-//        String str = "http://maps.googleapis.com/maps/api/geocode/json?address="
-//                + addy + "&sensor=true";
-//
-//        String ss = readWebService(str);
-//        JSONObject json;
-//        try {
-//
-//            String lat, lon;
-//            json = new JSONObject(ss);
-//            JSONObject geoMetryObject = new JSONObject();
-//            JSONObject locations = new JSONObject();
-//            JSONArray jarr = json.getJSONArray("results");
-//            int i;
-//            for (i = 0; i < jarr.length(); i++) {
-//                json = jarr.getJSONObject(i);
-//                geoMetryObject = json.getJSONObject("geometry");
-//                locations = geoMetryObject.getJSONObject("location");
-//                lat = locations.getString("lat");
-//                lon = locations.getString("lng");
-//
-//                locationPoint = Utils.getGeoPoint(Double.parseDouble(lat),
-//                        Double.parseDouble(lon));
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return locationPoint;
-//    }
-
 }
